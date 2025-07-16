0b5e7e0a6a68876f0ffd8c69601bbebf70b824f6
GofarP <gofarperdana83@gmail.com>
2025-07-16 10:53:03 +0700

    Deploy: 2025-07-16 10:53:03

--- build.sh
+++ build.sh
@@ -1,10 +1,12 @@
 echo "Building app..."
 npm run build
+
+rm dist/index.html
+
 cp -r dist/* .
+
 git add .
 git commit -m "Deploy: $(date '+%Y-%m-%d %H:%M:%S')"
 git push origin main
 
 echo "✅ Done: Vue app deployed to root of repo!"
-
-
